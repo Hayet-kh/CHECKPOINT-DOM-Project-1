addeefd2f94ca71e1e135316aa3a08ee84319769
Hayet-kh <61752528+Hayet-kh@users.noreply.github.com>
2024-10-27 18:50:57 +0100

    VOO - Initial Version

---
+++ products.js
@@ -0,0 +1,31 @@
+products = [
+	//PRODUCT 1
+	{
+		id: 0,
+		name: `Souris Logitech MX ERGO S`,
+		price: 119.99,
+		stock: 5,
+		shipping: 12,
+		img: `./assets/Product_01.webp`,
+	},
+
+	//PRODUCT 2
+	{
+		id: 1,
+		name: `Clavier Logitech K860`,
+		price: 129.0,
+		stock: 3,
+		shipping: 8,
+		img: `./assets/Product_02.webp`,
+	},
+
+	//PRODUCT 3
+	{
+		id: 2,
+		name: `Logitech Stylet (USB-C)`,
+		price: 89.99,
+		stock: 2,
+		shipping: 10,
+		img: `./assets/Product_03.webp`,
+	},
+];
